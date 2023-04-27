79cd5aec323bf934e68d5b073e8c710ef4e0e616
Alan Reyes <alan.reyes@mongodb.com>
2023-04-27 12:26:56 -0600

    Replica configuration script uses the hostname of the connected node instead of a hardcoded name.

--- config-replica.js
+++ config-replica.js
@@ -1,6 +1,9 @@
+var hostname = db.hostInfo().system.hostname
+console.log("Setting up MongoDB replica set with hostname: ", hostname)
+
 rsconf = {
   _id: "rs0",
-  members: [{ _id: 0, host: "mongo1:27017", priority: 1.0 }],
+  members: [{ _id: 0, host: `${hostname}:27017`, priority: 1.0 }],
 };
 rs.initiate(rsconf);
 rs.status();

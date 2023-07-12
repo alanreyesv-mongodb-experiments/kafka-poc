20a1d337cf3a07fdc841654fffa264d4ccaf0109
Alan Reyes <alan.reyes@mongodb.com>
2023-07-12 08:28:16 -0600

    Adds fallback force reconfig of MongoDB for cases where the containers are recreated

--- config-replica.js
+++ config-replica.js
@@ -5,5 +5,13 @@ rsconf = {
   _id: "rs0",
   members: [{ _id: 0, host: `${hostname}:27017`, priority: 1.0 }],
 };
-rs.initiate(rsconf);
+
+try {
+  // Initialize replica set
+  rs.initiate(rsconf);
+} catch (ex) {
+  // If already initialized, force reconfiguration
+  // (useful in case of container recreation when the container name would change)
+  rs.reconfig(rsconf, {force: true})
+}
 rs.status();
